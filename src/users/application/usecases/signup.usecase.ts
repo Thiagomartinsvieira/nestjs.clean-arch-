7f0592c9de2a981f3f4e1a5deb5f962fbee8a317
Thiago Martins Vieira <thiago.devstack@gmail.com>
2026-05-11 20:47:35 -0300

    feat: add BadRequestError class and integrate it into SignupUseCase for input validation

--- src/users/application/usecases/signup.usecase.ts
+++ src/users/application/usecases/signup.usecase.ts
@@ -1,3 +1,7 @@
+import { UserRepository } from '@/users/domain/repositories/user.repository';
+import { BadRequestError } from '../errors/bad-request-errror';
+import { UserEntity } from '@/users/domain/entities/user.entity';
+
 export namespace SignupUseCase {
   export type Input = {
     name: string;
@@ -14,8 +18,22 @@ export namespace SignupUseCase {
   };
 
   export class useCase {
+    constructor(private userRepository: UserRepository.Repository) {}
+
     async execute({ input: Input }): Promise<Output> {
-      
+      const { name, email, password } = Input;
+
+      if (!name || !email || !password) {
+        throw new BadRequestError('Name, email and password are required');
+      }
+
+      await this.userRepository.emailExists(email);
+
+      const entity = new UserEntity(Input);
+
+      await this.userRepository.insert(entity);
+
+      return entity.toJSON();
     }
   }
 }
