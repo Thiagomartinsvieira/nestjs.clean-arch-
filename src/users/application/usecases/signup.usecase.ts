4df1f4bb9bc827fde3aa769706639e88ce02bf77
Thiago Martins Vieira <thiago.devstack@gmail.com>
2026-05-13 20:56:30 -0300

    feat: enhance SignupUseCase to include password hashing with HashProvider

--- src/users/application/usecases/signup.usecase.ts
+++ src/users/application/usecases/signup.usecase.ts
@@ -1,6 +1,7 @@
 import { UserRepository } from '@/users/domain/repositories/user.repository';
 import { BadRequestError } from '../errors/bad-request-errror';
 import { UserEntity } from '@/users/domain/entities/user.entity';
+import { HashProvider } from '@/shared/application/providers/hash-provider';
 
 export namespace SignupUseCase {
   export type Input = {
@@ -18,7 +19,10 @@ export namespace SignupUseCase {
   };
 
   export class useCase {
-    constructor(private userRepository: UserRepository.Repository) {}
+    constructor(
+      private userRepository: UserRepository.Repository,
+      private hashProvider: HashProvider,
+    ) {}
 
     async execute({ input: Input }): Promise<Output> {
       const { name, email, password } = Input;
@@ -29,7 +33,11 @@ export namespace SignupUseCase {
 
       await this.userRepository.emailExists(email);
 
-      const entity = new UserEntity(Input);
+      const hashPassword = await this.hashProvider.generateHash(password);
+
+      const entity = new UserEntity(
+        Object.assign(Input, { password: hashPassword }),
+      );
 
       await this.userRepository.insert(entity);
 
