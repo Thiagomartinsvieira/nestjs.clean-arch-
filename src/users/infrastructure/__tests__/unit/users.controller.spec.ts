39f6f9cf7bd89f3265c59338d3e51e1428835328
Thiago Martins Vieira <thiago.devstack@gmail.com>
2026-06-17 23:12:55 -0300

    feat: add unit test for deleting a user in UsersController

--- src/users/infrastructure/__tests__/unit/users.controller.spec.ts
+++ src/users/infrastructure/__tests__/unit/users.controller.spec.ts
@@ -104,4 +104,16 @@ describe('UsersController', () => {
       ...input,
     });
   });
+
+  it('should delete a user', async () => {
+    const output: undefined = undefined;
+    const mockDeleteUserUseCase = {
+      execute: jest.fn().mockResolvedValue(Promise.resolve(output)),
+    };
+
+    sut['deleteUserUseCase'] = mockDeleteUserUseCase as any;
+    const result = await sut.remove(id);
+    expect(result).toStrictEqual(result);
+    expect(mockDeleteUserUseCase.execute).toHaveBeenCalledWith({ id });
+  });
 });
